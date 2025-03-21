bfd2c1c98d468934638fd43b3485f0a65113dd7f
Ishma Hafeez <ishmahafeez@gmail.com>
2025-03-21 17:47:26 +0500

    description updated

--- src/components/sections/FAQ.tsx
+++ src/components/sections/FAQ.tsx
@@ -8,21 +8,29 @@ export const FAQ: React.FC = () => {
       <h2 className="text-center text-[28px] font-bold mb-[30px]">
         Frequently asked questions
       </h2>
-        <div className="w-full max-w-[838px] max-sm:w-[350px]">        <FAQItem 
+        <div className="w-full max-w-[838px] max-sm:w-[350px]">        
+          <FAQItem 
           question="What is the process to Map Of US?" 
-          answer="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          answer="After placing your order, you’ll receive a Questionnaire via email with guided prompts to share stories, photos, and details about milestones, special places, trips, hobbies, pets, kids, and cherished memories. This ensures your map reflects what matters most. (If this is a gift, you can forward it to the recipient to fill out.)
+          Once your turn in the queue arrives, I'll send an initial outline of your map for review, allowing us to discuss any changes. Then, I'll begin creating your one-of-a-kind piece!
+          You’ll receive a Finished Draft 1-2 weeks before your promised ship date, where you can request final edits. If changes are needed, please allow 2-3 days for adjustments. After that, you’ll receive a High-Resolution file, and for US orders, your physical print will be on its way! ❤️"
         />
         <FAQItem 
           question="What makes Map Of Us stand out from the crowd?" 
-          answer="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          answer="✅ Handmade & Minimalist – A custom artistic sketch, not just a generic gift.
+                  ✅ More Than Photos – Captures emotions, milestones, and shared memories.
+                  ✅ One-of-a-Kind – Every map is uniquely crafted for each couple.
+                  ✅ Sentimental Keepsake – A timeless reminder of love and connection.
+
+                  It’s not just a map—it’s your story in art.."
         />
         <FAQItem 
           question='What do you mean by "Locations"?' 
-          answer="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          answer="Locations refer to meaningful places in your journey—where you met, your first home, favorite date spots, or any place with special memories that make your story unique."
         />
         <FAQItem
           question="How long till my Map of Us arrives?"
-          answer="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          answer="It takes about a week to sketch, create the layout, finalize the draft, and make any requested changes before it’s ready for delivery."
         />
       </div>
     </section>
