7b4e3a90d19cf65e14aebcca8ad6e1482d3768ec
Ishma Hafeez <ishmahafeez@gmail.com>
2025-03-23 17:18:48 +0500

    CLEANED

--- src/components/ui/TemplateCard.tsx
+++ src/components/ui/TemplateCard.tsx
@@ -16,7 +16,7 @@ export const TemplateCard: React.FC<TemplateCardProps> = ({
   const isMobile = useIsMobile();
   
   return (
-   <div className="flex-1 bg-[#EBE9E0] flex flex-col items-start p-4 md:p-5 lg:p-[35px] h-full max-w-[400px] sm:max-w-none mx-auto sm:mx-0">
+   <div className="flex-1 bg-[#EBE9E0] flex flex-col items-start p-4 md:p-5 lg:p-[35px] h-full max-w-[350px] sm:max-w-none mx-auto sm:mx-0">
      <div className="w-full flex justify-center">
        <img 
          src={image} 
@@ -24,10 +24,8 @@ export const TemplateCard: React.FC<TemplateCardProps> = ({
          className="w-full max-w-[264px] h-auto aspect-[264/292] mb-5 md:mb-[20px] lg:mb-[30px] object-cover"
        />
      </div>
-     <div className="w-full flex flex-col items-center sm:items-start">
-       <h3 className="text-xl font-bold mb-3 md:mb-[15px] px-2 sm:px-0">{title}</h3>
-       <div className="text-s text-left w-full px-2 sm:px-0">{description}</div>
-     </div>
+     <h3 className="text-xl font-bold mb-3 md:mb-[15px]">{title}</h3>
+     <div className="text-s text-left w-full">{description}</div>
    </div>
   );
 };
