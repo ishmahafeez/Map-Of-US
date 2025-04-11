3441823a1e72f0048687c354a5af8f57a5bed9a7
Ishma Hafeez <ishmahafeez@gmail.com>
2025-04-11 05:19:22 +0500

    removed analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/react";
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -23,7 +23,6 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-        <Analytics />
       </TooltipProvider>
     </HelmetProvider>
   </QueryClientProvider>
